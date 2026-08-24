0f2baa241af69ed1b970a54883ac9fbfb5b04a71
oka-wani <2cyscf4hms@privaterelay.appleid.com>
2026-08-25 03:02:39 +0900

    Harden custom worker entrypoint types

--- src/worker.ts
+++ src/worker.ts
@@ -2,7 +2,7 @@ import { handle } from "@astrojs/cloudflare/handler";
 export { ProposalWorkflow } from "./proposal-workflow";
 
 export default {
-  async fetch(request: Request, env: unknown, ctx: ExecutionContext) {
+  async fetch(request: Request, env: any, ctx: ExecutionContext) {
     return handle(request, env, ctx);
   },
-} satisfies ExportedHandler;
+} satisfies ExportedHandler<any>;

8055ad5a1ea10a565bd1aac048a731961435307d
oka-wani <2cyscf4hms@privaterelay.appleid.com>
2026-08-24 17:11:00 +0900

    Add Google Calendar API environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,5 +5,9 @@ declare namespace Cloudflare {
     RESEND_API_KEY: string;
     CONTACT_ADMIN_EMAIL: string;
     CONTACT_FROM_EMAIL: string;
+    GOOGLE_CALENDAR_CLIENT_ID: string;
+    GOOGLE_CALENDAR_CLIENT_SECRET: string;
+    GOOGLE_CALENDAR_REFRESH_TOKEN: string;
+    GOOGLE_CALENDAR_ID: string;
   }
 }
